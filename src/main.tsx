655a1ef2939b799effdab7dc3d15f5d600c0c54f
drvendasdev <luciano@drvendastreinamentos.com.br>
2026-01-07 08:15:40 -0300

    ....

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,15 @@ import App from './App.tsx'
 import './index.css'
 import { SystemCustomizationProvider } from './contexts/SystemCustomizationContext'
 
-// Remover favicons padrão do Lovable imediatamente
-const existingFavicons = document.querySelectorAll("link[rel*='icon'], link[rel*='shortcut']");
-existingFavicons.forEach(link => link.remove());
-// Adicionar favicon customizado após remover os padrões
-const link = document.createElement('link');
-link.rel = 'icon';
-link.type = 'image/x-icon';
-link.href = '/favicon.ico';
-document.head.appendChild(link);
+// // Remover favicons padrão do Lovable imediatamente
+// const existingFavicons = document.querySelectorAll("link[rel*='icon'], link[rel*='shortcut']");
+// existingFavicons.forEach(link => link.remove());
+// // Adicionar favicon customizado após remover os padrões
+// const link = document.createElement('link');
+// link.rel = 'icon';
+// link.type = 'image/x-icon';
+// link.href = '/favicon.ico';
+// document.head.appendChild(link);
 
 // Aplicar tema salvo imediatamente ao carregar
 const savedTheme = localStorage.getItem('theme');
